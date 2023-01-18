57cbf692537d75a07f85f97bed0eca4a9b75cbe9
VegadelaLyra <lyradeperseo@gmail.com>
2023-01-18 10:57:27 -0500

    Sequential test optimized

--- index.js
+++ index.js
@@ -4,5 +4,8 @@ import yargs from 'yargs'
 yargs(process.argv.slice(2))
 .usage('\nLook up the most advanced definition of a word in the Cambridge dictionary.')
 .command('$0', 'Search your word in Cambridge', ()=> {}, () => import('./src/commands/default.js'))
-.command('test', 'Test the speed of 10 scrapes', () => {}, () => import('./src/commands/test.js'))
+.command('sequentially', 'Test the speed of n scrapes', () => {}, () => import('./src/commands/test_sql.js'))
+.alias('sql', 'sequentially')
+.command('in parallel', 'Test 100 times concurrently', () => {}, () => import('./src/commands/parallel.js'))
+.alias('concurrently', 'in parallel')
 .argv
--- src/commands/test_sql.js
+++ src/commands/test_sql.js
@@ -2,25 +2,35 @@ import webScrape from "../scrapyWeb.js"
 
 // Time testing
 let n = 0, avg = [], userInput = process.argv.slice(3)[0]
+const cap = process.argv.slice(4)[0] ||  10
 if (!userInput) userInput = await import('./userInput.js').then(d => d.default())
 if (!/^[a-zA-Z]+$/.test(userInput)) {
     console.log('\nEnter ONE valid ENGLISH WORD\n') 
     process.exit()
+} 
+
+function endOfTest() {
+    avg = avg.reduce((acc, val) => acc + val, 0) / avg.length
+    console.log(
+        n, n <= 1 ? 'test' : 'tests', 'done.', 
+        'Word scraped from Cambridge dictionary in roughly', 
+        avg, 'ms'
+    ); process.exit()
 }
+
+process.on('SIGINT', endOfTest)
 
 do {
     const start = performance.now()
-    
+
     // Your code goes here
     await webScrape(userInput)
 
     const end = performance.now()
     
     const elapsedTime = end - start
-    console.log('Elapsed time: ', elapsedTime, 'ms')
+    console.log('Test n°', n + 1, 'Elapsed time: ', elapsedTime, 'ms')
     avg.push(elapsedTime); n++
-} while ( n < 10 )
+} while ( cap == '!' || n < cap)
 
-avg = avg.reduce((acc, val) => acc + val, 0) / avg.length
-console.log('Word scraped from Cambridge dictionary in roughly', avg, 'ms')
-process.exit()
+endOfTest()
