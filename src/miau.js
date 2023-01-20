c92671a9691c597d931fc67f0f520391eda16095
VegadelaLyra <lyradeperseo@gmail.com>
2023-01-20 08:31:31 -0500

    Refactoring our scrapy cambridge app to concurrent paradigm

--- src/miau.js
+++ src/miau.js
@@ -1,23 +1,18 @@
-// async function miau () {
-//     return {
-//         doc: 'awebo',
-//         jejeje: 234
-//     }
-// }
+// Time testing
+let lvl2 = ['A1', 'A1']
+let CEFR2 = ['C2', 'C1', 'B2', 'B1', 'A2']
+let cat = ['C', 'B', 'A']
+lvl2 = new Set(lvl2)
+console.log(lvl2);
+lvl2 = CEFR2.find(x => lvl2.has(x))
+console.log('lvl2', lvl2)
 
-// const [docky, vane] = await Promise.all([
-//     miau().then(x => x.doca),
-//     miau().then(x => x.jejeje)
-// ]).catch(console.error('miau miau'))
-
-// console.log({docky, vane})
-
-
-function a(miau = 10) {
-    return function b(wow = 20){
-        return function c(){
-            return console.log( [miau, wow, miau + wow] )
-        }
-    }
-}
-a()()()
+let miau = 3 - 4 ? console.log('miau', miau)
+: console.log('wow', miau)
+// // last approach
+// lvl = lvl.map(htmlEl => htmlEl.textContent)
+// let CEFR = ['C2', 'C1', 'B2', 'B1', 'A2', 'A1']
+// for (let i = 0; i < CEFR.length; i++) {
+//     if (!lvl.includes(CEFR[i])) continue
+//     lvl = lvl.filter(x => x == CEFR[i])
+//     break
