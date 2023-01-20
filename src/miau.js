da13737c10667a4ec607682cc86ab289987ef7b6
VegadelaLyra <lyradeperseo@gmail.com>
2023-01-20 14:42:40 -0500

    Refactoring our scrapy cambridge app to concurrent paradigm

--- src/miau.js
+++ src/miau.js
@@ -1,14 +1,8 @@
 // Time testing
-let lvl2 = ['A1', 'A1']
-let CEFR2 = ['C2', 'C1', 'B2', 'B1', 'A2']
-let cat = ['C', 'B', 'A']
-lvl2 = new Set(lvl2)
-console.log(lvl2);
-lvl2 = CEFR2.find(x => lvl2.has(x))
-console.log('lvl2', lvl2)
+let lvl2 = ['C2', 'C1', 'A1', 'A1', 'A2', 'B1', 'B1', 'A2', 'B1', 'B1', 'B2', 'B2']
+// lvl2 = new Set(lvl2)
+console.log(lvl2.sort().at(-1));
 
-let miau = 3 - 4 ? console.log('miau', miau)
-: console.log('wow', miau)
 // // last approach
 // lvl = lvl.map(htmlEl => htmlEl.textContent)
 // let CEFR = ['C2', 'C1', 'B2', 'B1', 'A2', 'A1']
--- src/scrapyWeb.js
+++ src/scrapyWeb.js
@@ -24,120 +24,67 @@ export default async function webScrape(userInput) {
 
 // START THE PARTY ! ~ CONCURRENT SIDE ~
 
-    const [wrd, IPA, PoS, [lvl, def, exp]] = await Promise.all([
+    await Promise.all([
         page.$eval('.dhw', word => word.textContent),
         page.$eval('.us .dpron', ipa => ipa.textContent.replaceAll('/', '')),
         page.$eval('.pos', pos => pos.textContent),
-        spot_lvl_def_exp()
-    ])
-    .then(() => {
-        browser.close() 
-        const cambridge = {
-        word: wrd,
-        IPA: IPA,
-        PoS: PoS,
-        level: lvl,
-        def: def.at(-1) == ' '
-        ? def.slice(0, -2)
-        : def,
-        exp: exp.at(-1) == '.' 
-        ? exp.trim().slice(0, -1)
-        : exp.trim()
-        }
-        console.log(cambridge)
-    }).catch(() => console
-    .log(`\n${userInput} is not available in the Cambridge dictionary\n`))
+        spot_lvl_def_exp(),
+    ]).then(values => {
+        browser.close()
+        return console.log({
+            word: values[0], 
+            IPA : values[1],
+            PoS : values[2],
+            CEFR: values[3][0],
+            def : values[3][1],
+            exp : values[3][2]
+        })
+    }).catch(err => console
+    .log(err, `\n${userInput} is not available in the Cambridge dictionary\n`))
     
-    
-
     // SCRAPE HIGHEST LEVEL, THEN SHORTEST DEF, THEN SHORTEST EXP 
     async function spot_lvl_def_exp() {
     // SCRAPING LEVEL
         // Does the word have CEFR levels at all?
-        let lvl = await page.$$('.dxref') 
-        if (lvl.length == 0
-        ) return await spot_shortest_def_exp(`.ddef_block:has(.db)`)
-        lvl = new Set(lvl.map(htmlEl => htmlEl.textContent))
-        // If the word have only one level, go with it
-        if (lvl.length == 1
-        ) return await spot_shortest_def_exp(`.ddef_block:has(.${lvl[0]})`)
-
+        let lvl = await page.$$eval(
+            '.dxref', promises => {
+                let lvl = promises.map(htmlEl => htmlEl.textContent)
+                return lvl.sort().at(-1) ?? ''
+            }
+        ); if (lvl == '') return await spot_shortest_def_exp(`.ddef_block:has(.db)`)
         // If word have various def levels, choose the highest one.
-        let CEFR = ['C2', 'C1', 'B2', 'B1', 'A2', 'A1']
-        lvl = CEFR.find(higher => lvl.has(higher))
-        spot_shortest_def_exp(`.ddef_block:has(.${lvl})`)
+        return spot_shortest_def_exp(`.ddef_block:has(.${lvl})`)
         // END OF SCRAPING LEVEL
     // SCRAPING DEFINITION && EXAMPLE IN PARALLEL
         async function spot_shortest_def_exp(level) {
             // getting all blocks that matches the scraped level
-            let block = await page.$$eval(level, block => {
-                // leaving only the block with the shortest definition
-                return block.reduce((a, b) => {
-                    return a.querySelector('.db').textContent.split(' ').length <=
-                    b.querySelector('.db').textContent.split(' ').length ? a : b
+            const shortestBlock = await page.$$eval(level, blocks => {
+                // getting the block with the shortest def
+                let defEx = blocks.map( block => {
+                    let exs = block.querySelectorAll('.dexamp'), exsArr = []
+                    if (exs) for (let ex of exs) exsArr.push(ex.textContent)
+
+                    return {
+                        def: block.querySelector('.db').textContent,
+                        exp: exsArr.length > 1 ? exsArr : !exsArr[0] ? '' : exsArr[0]
+                    }
+                })
+                return defEx.reduce((a, b) => {
+                    return a.def.split(' ').length 
+                    <= b.def.split(' ').length ? a : b
                 })
             })
             // getting shortest definition and example concurrently
             const [def, exp] = await Promise.all([
-                block.querySelector('.db').textContent,
-                Array.from(
-                    block.querySelectorAll('.dexamp')
-                    .values()).reduce((a, b) => { 
-                    return a.textContent.split(' ').length <=
-                    b.textContent.split(' ').length ? a : b
-                }).catch(() => '')
-            ])
-    // END OF SCRAPING DEFINITION
-    // SCRAPING EXAMPLE
-            // // getting shortest example
-            // try {
-            //     exp = Array.from(
-            //         block.querySelectorAll('.dexamp')
-            //         .values()).reduce((a, b) => { 
-            //         return a.textContent.split(' ').length <=
-            //         b.textContent.split(' ').length ? a : b
-            //     })
-            // } catch { exp = '' }
-    // END OF SCRAPING EXAMPLE
-    // RETURNING LVL, DEF, EXP
-            return [lvl, def, exp]
+                shortestBlock.def.at(-1) == ' '
+                ? shortestBlock.def.slice(0, -2) 
+                : shortestBlock.def,
+                shortestBlock.exp instanceof Array 
+                ? shortestBlock.exp.reduce((a, b) => { 
+                    return a.split(' ').length <=
+                    b.split(' ').length ? a : b
+                }) : shortestBlock.exp
+            ]); return [lvl, def, exp.at(-1) == '.' ? exp.slice(0, -1) : exp]
         }
     }
-    // scrape the shortest definition & example of the word
-    // CEFR = lvl == '' ? '.ddef_block:has(.db)' : `.ddef_block:has(.${lvl})` 
-    // const leveled_blocks = await page.$$eval(CEFR, blocks => {
-    //     let shortest_def = blocks.reduce((a, b) => {
-    //         return a.querySelector('.db').textContent.split(' ').length <= 
-    //         b.querySelector('.db').textContent.split(' ').length ? a : b 
-    //     }); let shortest_exp
-
-    //     try {
-    //         shortest_exp = Array.from(
-    //             shortest_def.querySelectorAll(
-    //             '.dexamp').values()).reduce((a, b) => { 
-    //             return a.textContent.split(' ').length <= 
-    //             b.textContent.split(' ').length ? a : b 
-    //         })
-    //     } catch { shortest_exp = '' }
-        
-    //     return [
-
-    //         shortest_def.querySelector('.db').textContent,
-    //         shortest_exp?.textContent || ''
-    //     ]
-    // })
-
-    // address output
-    // const cambridge = {
-    //     word: wrd,
-    //     IPA: await page.$eval('.us .dpron', ipa => ipa.textContent.replaceAll('/', '')), 
-    //     PoS: await page.$eval('.pos', pos => pos.textContent),
-    //     level: lvl,
-    //     def: leveled_blocks.def.at(-1) == ' '
-    //     ? leveled_blocks.def.slice(0, -2)
-    //     : leveled_blocks.def,
-    //     exp: leveled_blocks.exp.at(-1) == '.' 
-    //     ? leveled_blocks.exp.trim().slice(0, -1)
-    //     : leveled_blocks.exp.trim()
-    // }; 
 }
