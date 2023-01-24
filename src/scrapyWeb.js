ba221b41d7d7317f61b0aedd6c2d897a551febc9
VegadelaLyra <lyradeperseo@gmail.com>
2023-01-24 17:41:43 -0500

    TIME LESSEN IN UP TO 40% WITH REFACTORING

--- src/scrapyWeb.js
+++ src/scrapyWeb.js
@@ -22,11 +22,10 @@ export default async function webScrape(userInput, test = false) {
         exp  : exp
     })
 
-    function ScrapingCambridge(){
+    async function ScrapingCambridge(){
         let CEFR = $('.dxref')
-        CEFR = !!CEFR.length 
-        ? CEFR.text().match(/.{1,2}/g).sort().at(-1)
-        : ''
+        CEFR = !CEFR.length ? '' 
+        : CEFR.text().match(/.{1,2}/g).sort().at(-1)
             
         // Gets the top CEFR level block with the shortest definition
         let lvl = !CEFR ? '' : `:has(.${CEFR})` 
@@ -35,7 +34,7 @@ export default async function webScrape(userInput, test = false) {
             def: $(this).find('.def').text(), 
             exp: $(this).find('.dexamp')
                 .toArray().map(x => $(x).text()),
-            the: $(this).parent().parent().parent().prev()
+            the: $(this).parents().eq(2).prev()
             .map( function() { return {
                 ipa: $(this).find('.us .dpron').text(),
                 pos: $(this).find('.dpos:first').text()
@@ -44,26 +43,31 @@ export default async function webScrape(userInput, test = false) {
         a.def.split(' ').length <=
         b.def.split(' ').length ? a : b)
 
+        // GETTING ALL DATA IN PARALLEL
+        const [ipa, pos, def, exp] = await Promise.all([
         // Spot out the IPA and PoS of top level definition
-        let ipa = topBlock.the?.ipa.slice(1, -1) ?? '' 
-        let pos = topBlock.the?.pos ?? ''
-
+            topBlock.the?.ipa.slice(1, -1) ?? '',
+            topBlock.the?.pos ?? '',
+            getDf(),
+            getEx()
+        ])
         // Top level shortest definition
-        let def = topBlock.def
-        def = def.at(-2) == ':' 
-        ? def.slice(0, -2)
-        : def.trim()
-
+        function getDf() {
+            let def = topBlock.def
+            return def = def.at(-2) == ':' 
+            ? def.slice(0, -2)
+            : def.trim()
+        }
         // Get the shortest example if any
-        let exp = topBlock.exp
-        exp = !exp.length ? ''
-        : exp.reduce((a, b) => 
-        a.split(' ').length <= 
-        b.split(' ').length ? a : b)  
-        exp = exp.at(-1) == '.' 
-        ? exp.slice(0, -1)
-        : exp.trim()
-
-        return [ipa, pos, CEFR, def, exp]        // VICTORY!!!
+        function getEx() {
+            let exp = topBlock.exp
+            exp = !exp.length ? ''
+            : exp.reduce((a, b) => 
+            a.split(' ').length <= 
+            b.split(' ').length ? a : b)  
+            return exp = exp.at(-1) == '.' 
+            ? exp.slice(0, -1)
+            : exp.trim()
+        }; return [ipa, pos, CEFR, def, exp]        // VICTORY!!!
     }
 }
