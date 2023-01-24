281704f7b502b2289e4efa402118e98957c9d298
VegadelaLyra <lyradeperseo@gmail.com>
2023-01-24 13:50:58 -0500

    Scrapy app refactored from puppeteer to cheerio-axios

--- src/scrapyWeb.js
+++ src/scrapyWeb.js
@@ -5,20 +5,50 @@ import { load } from 'cheerio'
 export default async function webScrape(userInput, test = false) {
     // send the HTTP get request with axios library, parse the data with cheerio
     const url = `https://dictionary.cambridge.org/dictionary/english/${userInput}`
-    const res = await axios.get(url)
-    let $ = load(res.data)
-    let page = $('#page-content').html()
-    $ = load(page)
+    const res = await axios.get(url), $ = load(res.data)
 
     // retrieve all the desired data with high-level selectors in parallel
-    const [wrd, ipa, PoS, lvl, def, exp] = await Promise.all([
+    const [wrd, [ipa, PoS, lvl, def, exp]] = await Promise.all([
         $('#cald4-1+ .dpos-h .dpos-h_hw').text(), // word, phrase or idiom name
-        $('.us .lpl-1').text(), // IPA of the highest level
-        $('.pos').first().text(), // Position of Speech of the highest level
-        $('.A2').first().text(), // highest available CEFR (Common European Framework Reference) level
-        $('.db').first().text(), // shortest definition with the highest CEFR level if any
-        $('.dexamp').first().text(), // shortest example of the shortest definition with highest level
+        assBigFnThatScrapesAll()
     ])
+
+    function assBigFnThatScrapesAll(){
+        let CEFR = $('.dxref')
+        CEFR = !!CEFR.length 
+        ? CEFR.text().match(/.{1,2}/g).sort().at(-1)
+        : ''
+            
+        // Gets the top CEFR level block with the shortest definition
+        let lvl = !CEFR ? '' : `:has(.${CEFR})` 
+        let topBlock = $(`.dsense_b > .def-block${lvl}, .phrase-block${lvl}`)
+        .map( function() { return {
+            def: $(this).find('.def').text(), 
+            exp: $(this).find('.dexamp')
+                .toArray().map(x => $(x).text())
+        }}).toArray().reduce((a, b) => 
+        a.def.split(' ').length <=
+        b.def.split(' ').length ? a : b)
+
+        // Top level shortest definition
+        let def = topBlock.def
+        def = def.at(-2) == ':' 
+        ? def.slice(0, -2)
+        : def.trim()
+
+        // Get the shortest example if any
+        let exp = topBlock.exp
+        exp = !exp.length ? ''
+        : exp.reduce((a, b) => 
+        a.split(' ').length <= 
+        b.split(' ').length ? a : b)  
+        exp = exp.at(-1) == '.' 
+        ? exp.slice(0, -1)
+        : exp.trim()
+
+        console.log(topBlock)
+        return [1,2, CEFR, def, exp]        
+    }
 
     console.log({
         word: wrd,
