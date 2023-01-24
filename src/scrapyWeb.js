1f9f7c00ccc6f4259c3875b9d8f60d6d2e71531c
VegadelaLyra <lyradeperseo@gmail.com>
2023-01-23 19:15:44 -0500

    is cheerio axios faster? XD

--- src/scrapyWeb.js
+++ src/scrapyWeb.js
@@ -3,23 +3,27 @@ import { load } from 'cheerio'
 
 // web scrape your word data from Cambridge dictionary
 export default async function webScrape(userInput, test = false) {
+    // send the HTTP get request with axios library, parse the data with cheerio
     const url = `https://dictionary.cambridge.org/dictionary/english/${userInput}`
-    const res = await axios.get(url)
-    const html = res.data
-    const $ = load(html)
+    const res = await axios.get(url, {
+        'Accept-Encoding': 'gzip,deflate',
+    }); const $ = load(res.data)
 
-    const [wrd, ipa, PoS, def, exp] = await Promise.all([
-        $('.dhw').text(),
-        $('.us .dpron').text(),
-        $('.pos').text(),
-        $('.db').text(),
-        $('.dexamp').text(),
+    // retrieve all the desired data with high-level selectors in parallel
+    const [wrd, ipa, PoS, lvl, def, exp] = await Promise.all([
+        $('#cald4-1+ .dpos-h .dpos-h_hw').text(), // word, phrase or idiom name
+        $('<dsv<weg>').text(), // IPA of the highest level
+        $('.pos').first().text(), // Position of Speech of the highest level
+        $('.A2').first().text(), // highest available CEFR (Common European Framework Reference) level
+        $('.db').first().text(), // shortest definition with the highest CEFR level if any
+        $('.dexamp').first().text(), // shortest example of the shortest definition with highest level
     ])
 
     console.log({
         word: wrd,
         IPA: ipa,
         PoS: PoS,
+        lvl: lvl,
         def: def,
         exp: exp
     })
