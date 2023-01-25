f8761863f0889623f4985f111d6ff57ae859e7d6
VegadelaLyra <lyradeperseo@gmail.com>
2023-01-24 23:53:57 -0500

    TIME LESSEN IN UP TO 40% WITH REFACTORING

--- src/scrapyWeb.js
+++ src/scrapyWeb.js
@@ -44,7 +44,9 @@ export default async function webScrape(userInput, test = false) {
                 .toArray().map(x => $(x).text()),
             the: $(this).parents().eq(2).prev()
             .map( function() { return {
-                ipa: $(this).find('.us .dpron, .uk .dpron').text(),
+                ipa: !!$(this).has('.us .dpron').length 
+                ? $(this).find('.us .dpron').text()
+                : $(this).find('.uk .dpron').text(),
                 pos: $(this).find('.dpos:first').text()
             }}).toArray()[0]
         }}).toArray().reduce((a, b) => 
