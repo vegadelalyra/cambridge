48d1de5c57570717d3c81aa9a538a909e81237f0
VegadelaLyra <lyradeperseo@gmail.com>
2023-01-24 20:17:58 -0500

    TIME LESSEN IN UP TO 40% WITH REFACTORING

--- src/cache/hashTable.js
+++ src/cache/hashTable.js
@@ -1,9 +1,1 @@
 const pedia = {}; export { pedia }
-pedia.pursuit = {"wrd":"pursuit","IPA":"pɚˈsuːt","PoS":"noun","lvl":"C2","def":"the act of trying to achieve a plan, activity, or situation, usually over a long period of time","exp":"the pursuit of happiness"}
-pedia.introduction = {"wrd":"introduction","IPA":"ˌɪn.trəˈdʌk.ʃən","PoS":"noun","lvl":"C1","def":"the first time someone experiences something","exp":"That trip was my introduction to winter sports"}
-pedia.epic = {"wrd":"epic","IPA":"ˈep.ɪk","PoS":"noun","lvl":"","def":"extremely large","exp":"The problem of inflation has reached epic proportions"}
-pedia.profiler = {"wrd":"profiler","IPA":"ˈproʊ.faɪ.lɚ","PoS":"noun","lvl":"","def":"a computer program that checks and reports on how another program is working","exp":"A coverage profiler lets you analyse the performance of your code, and detect code that is never called"}
-pedia.length = {"wrd":"length","IPA":"leŋθ","PoS":"noun","lvl":"C2","def":"for a long time","exp":""}
-pedia.attired = {"wrd":"attired","IPA":"əˈtaɪəd","PoS":"adjective","lvl":"","def":"dressed in clothes of a particular type","exp":"She was attired from head to foot in black"}
-pedia.milk = {"wrd":"milk","IPA":"mɪlk","PoS":"noun","lvl":"A1","def":"the white liquid produced by cows, goats, and sheep and used by humans as a drink or for making butter, cheese, etc.","exp":"cow's/goat's milk"}
-pedia.lessen = {"word":"lessen","IPA":"ˈles.ən","PoS":"verb","lvl":"C1","def":"If something lessens or is lessened, it becomes less strong","exp":"A healthy diet can lessen the risk of heart disease"}
