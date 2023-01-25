f7a89e909439ae7203bf4b9e656eae090aa96595
VegadelaLyra <lyradeperseo@gmail.com>
2023-01-24 23:52:31 -0500

    TIME LESSEN IN UP TO 40% WITH REFACTORING

--- src/cache/hashTable.js
+++ src/cache/hashTable.js
@@ -1,2 +1,1 @@
 const pedia = {}; export { pedia }
-pedia.suite = {"word":"suite","IPA":"","PoS":"noun","lvl":"","def":"a set of related software products","exp":"We sell the popular Microsoft Office suite of applications"}
